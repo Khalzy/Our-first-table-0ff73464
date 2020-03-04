67e6c89eb50b154cd4f57edcd809b1967b5bac75
Candace <2100978@talnet.nl>
2020-03-04 15:31:54 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,3 +1,23 @@
-CREATE TABLE planeten (
-	naam INT
+CREATE TABLE planeten(
+	naam varchar(20)
+);
+
+
+INSERT INTO planeten VALUES(
+"Zon"
+);
+
+
+INSERT INTO planeten VALUES(
+"Mars"
+);
+
+INSERT INTO planeten VALUES(
+"Aarde"
+);
+INSERT INTO planeten VALUES(
+"Venus"
+);
+INSERT INTO planeten VALUES(
+"Mercuius"
 );
